23a439ba2e8180d2f2df012b89b2e9e4d4020ee2
Rohit Deshpande <rohitsay89@gmail.com>
2015-10-18 01:31:25 -0700

    Comments updated

--- least_num_macro.c
+++ least_num_macro.c
@@ -1,7 +1,12 @@
-  #include <stdio.h>
-  #define min(a, b)	((a<b)?a:b)			//macro for minimum of 2 numbers //
-  int main(){
-    int a = 0, b = 0;
+/* Program to find the Least number using Macro
+   Written by: ROHIT DESHPANDE
+   date: 10/18/2015
+*/
+
+#include <stdio.h>
+#define min(a, b)	((a<b)?a:b)			/* macro for minimum of 2 numbers */
+int main(){
+    	int a = 0, b = 0;
   	printf("To find the minimum of two numbers using Macro \n");
   	printf("Enter first number : \n");
   	scanf("%d", &a);
@@ -10,4 +15,4 @@
   	printf("the minimum number is: %d \n", min(a, b));//
 	
 	return 0;
-	}
+}
