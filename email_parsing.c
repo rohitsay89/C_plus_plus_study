58edb0abec7bee8c125e568fc8b76a154a5fd6c0
Rohit Deshpande <rohitsay89@gmail.com>
2015-10-20 00:29:13 -0700

    Comments updated

--- email_parsing.c
+++ email_parsing.c
@@ -6,25 +6,25 @@
 #include <stdlib.h>
 
 int main(){
-	FILE *fp = NULL;								// file pointer
-	char *str = {0};								// character pointer
-	int c=1;										// count for number of email address
-	fp = fopen("test.txt", "r");				// open the file
+	FILE *fp = NULL;					/* file pointer */
+	char *str = {0};					/* character pointer */
+	int c=1;						/* count for number of email address */
+	fp = fopen("test.txt", "r");				/* open the file */
 	int n=0;
-	if(fp == NULL){									// check for availability of file
+	if(fp == NULL){						/* check for availability of file */
 		printf("File cannot be opened");}
-	do{												// do while loop till end of file
-		str = (char*)malloc(1000);					// allocate 1000 bytes for the string, depending in the file size
-		fscanf(fp,"%s", str);						// read form the file and store the string(word) in the string 'str'
-	    for(n = 0; n<30; n++){						// scan for a letter in the string
-	    	if(str[n] == '@'){						// if the letter is '@'
-	    		printf("%d: %s \n",c,str);			// print the string
-	    		c++;								// increment a counter
-	    		continue;							// continue for next line
+	do{							/* do while loop till end of file */
+		str = (char*)malloc(1000);			/* allocate 1000 bytes for the string, depending in the file size */
+		fscanf(fp,"%s", str);				/* read form the file and store the string(word) in the string 'str' */
+	    for(n = 0; n<30; n++){				/* scan for a letter in the string */
+	    	if(str[n] == '@'){				/* if the letter is '@' */
+	    		printf("%d: %s \n",c,str);		/* print the string */
+	    		c++;					/* increment a counter */
+	    		continue;				/* continue for next line */
 	    	}
 	      }
-	   }while( !feof(fp) );							// check for end of file
-	fclose(fp);										// close the file
+	   }while( !feof(fp) );					/* check for end of file */
+	fclose(fp);						/* close the file */
 	printf("There are %d number of emails in this file", c-1);
-	return 0;										// print the number of email addresses found
+	return 0;						/* print the number of email addresses found */
 }
