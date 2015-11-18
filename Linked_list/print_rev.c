715e5c7fc327672e3d49114308816e86cb14830f
Rohit Deshpande <rohitsay89@gmail.com>
2015-11-18 13:43:21 -0800

    comments update

--- Linked_list/print_rev.c
+++ Linked_list/print_rev.c
@@ -1,3 +1,8 @@
+/*
+  This code is written for printing a linked list in reverse or forward
+  without actually changing the links
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -23,7 +28,7 @@ void Print(struct Node *p){
 	Print(p->next);					/* Forward print */
 }
 
-struct Node* Insert(struct Node *head, int data){
+struct Node* Insert(struct Node *head, int data){	/* function to fill the list by inserting data at end */
 	struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
 	temp->data = data;
 	temp->next = NULL;
@@ -42,13 +47,13 @@ struct Node* Insert(struct Node *head, int data){
 
 void main(){
 	struct Node *head = NULL;
-	head = Insert(head, 2);
-	head = Insert(head, 4);
+	head = Insert(head, 2);				/* fill the linked list by inserting at end */
+	head = Insert(head, 4);				
 	head = Insert(head, 6);
 	head = Insert(head, 8);
 	head = Insert(head, 726);
 
-	Print(head);
-	Reverse_Print(head);
+	Print(head);					/* Print elements in forward direction */
+	Reverse_Print(head);				/* print elements in reverse direction */
 	
 }
