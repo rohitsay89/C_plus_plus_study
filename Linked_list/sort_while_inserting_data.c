20a97c30726013b3e14d02dd62520fbd9a90e2df
Rohit Deshpande <rohitsay89@gmail.com>
2016-01-29 21:09:39 -0800

    comments made

--- Linked_list/sort_while_inserting_data.c
+++ Linked_list/sort_while_inserting_data.c
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node{
+struct Node{					/* node structure decleration */
 	int data;
 	struct Node *next;
 };
 
 struct Node *head = NULL;
 
-void Insert_head(int n){
+void Insert_head(int n){			/* function for inserting a node in the begining */
 	struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
 	temp->data = n;
 	temp->next = head;
@@ -19,7 +19,7 @@ void Insert_head(int n){
 	//printf("inserted data at end is: %d \n", temp->data);
 }
 
-void Insert_end(int n){
+void Insert_end(int n){				/* subroutine for inserting a node at the end */
 	struct Node *temp;
 	temp = head;
 	while(temp->next!=NULL){
@@ -32,36 +32,35 @@ void Insert_end(int n){
 	//printf("inserted data at end is: %d \n", temp1->data);
 }
 
-void Insert(int n){
-	int pos=0, i=0;
-	struct Node *temp, *prev;
-	prev = NULL;
-	temp = head;
-	if(temp == NULL){
+void Insert(int n){				/* Insert main function */
+	struct Node *temp, *prev;		/* local node */
+	prev = NULL;				/* a struct pointer to point to previous node */
+	temp = head;				
+	if(temp == NULL){			/* if the list is empty then make a node */
 		temp = (struct Node*)malloc(sizeof(struct Node));
 		temp->data = n;
 		temp->next = NULL;
 		head = temp;
 		return;
 	}
-	else if(temp->next == NULL){
+	else if(temp->next == NULL){		/* if list has single node then comapre data with that node */
 		if(n<temp->data){
 			//printf("If single element in the list, n < temp->data \n");
-			Insert_head(n);
+			Insert_head(n);		/* if data is less than node data insert in begining */
 			return;
 		}
-		else{
+		else{				
 			//printf("If single element in the list, n> temp->data \n");
-			Insert_end(n);
+			Insert_end(n);		/* if data is more than node data insert at end */
 			return;
 		}
 	}
-	else{
-		if(n<temp->data){
+	else{					/* else go through the list and place the data at proper location */
+		if(n<temp->data){		/* if the 1st node data is more than inserted data then insert at head */
 			Insert_head(n);
 			return;
 		}
-		while(temp!=NULL){
+		while(temp!=NULL){		/* traverse through the list and insert before greater node */
 			//printf("To insert node in between %d \n", n);
 			if(n<temp->data){
 				struct Node *temp1 = (struct Node*)malloc(sizeof(struct Node));
@@ -73,11 +72,11 @@ void Insert(int n){
 			prev = temp;
 			temp=temp->next;
 		}
-		Insert_end(n);
+		Insert_end(n);			/* If the inserted data is more than data of all nodes then insert the node at end */
 	}
 }
 
-void Print(){
+void Print(){					/* print the whole list */
 	struct Node *temp;
 	temp = head;
 	while(temp!=NULL){
@@ -90,11 +89,11 @@ int main(){
 	int i=0,n=0;
 	printf("The program to sort while inserting in a linked-list \n");
 	printf("Inert 10 elements to populate the list of 10 nodes: \n");
-	for(i=0;i<10;i++){
-		printf("Enter node: ");
-		scanf("%d", &n);
-		Insert(n);
+	for(i=0;i<10;i++){			/* make a linked list of 10 nodes */
+		printf("Enter node: ");		
+		scanf("%d", &n);		/* Prompt to user for inserting the node */
+		Insert(n);			/* invoke the insert function */
 	}
-	Print();
+	Print();				/* Print the whole list */
 	return 0;
 }
